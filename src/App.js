b59b0a589781704892531f853b7dfca02c45d1f9
Mohtasim Chowdhury <mohtasim317@hotmail.com>
2020-09-12 15:32:02 -0400

    Organized file structure and successfully fetching data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Header from "./components/Header";
 
 function App() {
+  const [fetched, updateFetched] = useState(false);
+  const [data, updateData] = useState([]);
+
+  const fetchData = async (url) => {
+    try {
+      const info = await fetch(url);
+      const parsed = await info.json();
+      console.log(parsed);
+      updateFetched(true);
+      updateData(parsed);
+    } catch (err) {
+      return err;
+    }
+  };
+
+  useEffect(() => {
+    fetchData("https://5f5aebbb044570001674c45d.mockapi.io/genity/data");
+  }, []);
+
   return (
     <div className="App">
-      <p>sup</p>
+      {fetched ? (
+        <div>
+          <Header />
+          {data.map((item, index) => {
+            return (
+              <div>
+                <h1>{item.title}</h1>
+                <h2>{item.secondary}</h2>
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        <h1>waiting</h1>
+      )}
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h2>hello</h2>
+    </div>
+  );
+};
+
+export default Header;
